9b8ae2153716b8bb89a5dd5c0822f64ec36fea66
ye2733 <ye2733@naver.com>
2022-05-03 11:48:09 +0900

    220503 오전 수업

---
+++ basic/gugudan.py
@@ -0,0 +1,27 @@
+#구구단 프로그램
+# 2x1 = 2 ... 2x9=18   X x Y = XY
+# 3x1 = 3 ... 3x9=27
+#             9x9=81
+'''
+print('----구구단 프로그램----')
+
+for x in range(2, 10):
+    for y in range(1, 10):
+        print(f'{x}x{y}={x*y:2d}', end=' ')       # end = '' 로 인해 한줄로 나타남
+
+'''
+
+print('----구구단 프로그램----')
+
+for x in range(2, 10):
+    for y in range(1, 10):
+        print(f'{x}x{y}={x*y:2d}', end=' ')         
+    print()  # 단마다 줄맞추기 위해서 
+
+print('----구구단 프로그램----')
+
+for x in range(2, 10):
+    print(f'{x}단 시작')
+    for y in range(1, 10):
+        print(f'{x}x{y}={x*y:2d}', end=' ')         
+    print()  # 단마다 줄맞추기 위해서 
--- basic/while_02.py
+++ basic/while_02.py
@@ -3,5 +3,5 @@ num = 1
 
 while True:
     print(f'{num}')
-    num += 1
+    num += 1            #정지 : ctrl + c
     
