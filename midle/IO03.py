12b325413e8d00d7e37f68f514929e60df01ce7c
ye2733 <ye2733@naver.com>
2022-05-04 11:45:00 +0900

    220504 내일은 쉬는날

---
+++ midle/IO03.py
@@ -0,0 +1,20 @@
+# 두개의 값을 입력받아 두개의 변수에 나눠담기
+# x, y,z = input('두개의 단어를 입력하세요(구분자는 /) > ').split('/')
+
+# print(x)
+# print(y)
+# print(z)             # 변수가 3개면 3개의 값을 넣어야 함
+
+
+# x, y= input('두개의 단어를 입력하세요(구분자는 /) > ').split('/')
+# print(int(x) * int(y))
+
+
+# x, y= map(float, input('두개의 단어를 입력하세요(구분자는 /) > ').split('/'))
+
+# print(x * y)
+
+
+# Escape 문자
+greeting = "Hello, I\'m a teacher. \"Bye~\" \\ "
+print(greeting)
